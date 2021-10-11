d73e5b4bcad49e1b9f7282e0aaa962e4e3c43714
marcoSven <me@marcosven.com>
2021-10-11 15:00:16 -0700

    Updates

---
+++ precache-manifest.45159e9db6c43f0f81778156770d06cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "181565fd55264175ec28068bfde12d9f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "98d5bd3ba262d1b65b53",
+    "url": "./static/js/2.1dd0ed19.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.1dd0ed19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b90b64da2abeb3905ffa",
+    "url": "./static/js/main.92735e62.chunk.js"
+  },
+  {
+    "revision": "59bd054f3bde6b0e219b610549d13f6f",
+    "url": "./static/js/main.92735e62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ccfa42e65efb35f6a81",
+    "url": "./static/js/runtime-main.2b171fd4.js"
+  }
+]);
